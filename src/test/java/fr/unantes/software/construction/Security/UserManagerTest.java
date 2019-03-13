862bac6c17fc23824c928550481b70965aae8f36
Yves-Marie CARETTE <E155311R@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-13 12:15:07 +0100

    test pour la classe manager

--- src/test/java/fr/unantes/software/construction/Security/UserManagerTest.java
+++ src/test/java/fr/unantes/software/construction/Security/UserManagerTest.java
@@ -1,4 +1,34 @@
 package fr.unantes.software.construction.Security;
+import fr.unantes.software.construction.people.Person;
+import fr.unantes.software.construction.security.UserManager;
+import org.junit.jupiter.api.*;
+import java.io.InvalidClassException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class UserManagerTest {
+
+    UserManager Test1 = new UserManager();
+
+    public UserManagerTest() throws InvalidClassException {
+    }
+
+    @Test
+    public void testinsertion() throws InvalidClassException, NoSuchAlgorithmException {
+        Person personne1 = new Person("YM1", "agent");
+        Test1.addUser(personne1, "motdepasse");
+        assertTrue(Test1.hasUser(personne1));
+    }
+
+    @Test
+    public void testremove() throws InvalidClassException, NoSuchAlgorithmException {
+        Person personne2 = new Person("YM2","agent");
+        Test1.addUser(personne2, "motdepasse");
+        Test1.removeUser(personne2);
+        assertFalse(Test1.hasUser(personne2));
+    }
+
 }
