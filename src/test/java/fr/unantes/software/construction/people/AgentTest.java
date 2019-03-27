9c299aed2cf266f1a158b6aeab8add0ec25d5f28
Soline LECOMTE <E164462N@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-27 11:46:38 +0100

    Mise en place des tests pour la classe agent et administrateur

--- src/test/java/fr/unantes/software/construction/people/AgentTest.java
+++ src/test/java/fr/unantes/software/construction/people/AgentTest.java
@@ -1,4 +1,44 @@
 package fr.unantes.software.construction.people;
+import fr.unantes.software.construction.calendar.Calendar;
+import fr.unantes.software.construction.calendar.Travel;
+import fr.unantes.software.construction.people.Administrateur;
+import fr.unantes.software.construction.people.Agent;
+import fr.unantes.software.construction.people.Person;
+import fr.unantes.software.construction.security.GestionMdp;
+import fr.unantes.software.construction.security.UserManager;
+import org.junit.jupiter.api.*;
+import java.io.InvalidClassException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class AgentTest {
+
+    Agent p1 = new Agent("Paul");
+
+
+    public AgentTest() throws InvalidClassException {
+    }
+
+
+    @Test
+    public void testaddTravel() throws InvalidClassException {
+        Calendar c = new Calendar(p1);
+        Travel t = new Travel(c);
+        p1.addTravel(t);
+        assertTrue(p1.getCalendar().getTravels().contains(t));
+    }
+
+    @Test
+    public void testremoveTravel() throws NoSuchAlgorithmException, InvalidClassException {
+        Calendar c = new Calendar(p1);
+        Travel t = new Travel(c);
+        p1.addTravel(t);
+        p1.removeTravel(t);
+        assertFalse(p1.getCalendar().getTravels().contains(t));
+    }
+
+
 }
