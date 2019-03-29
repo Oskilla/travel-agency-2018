42656e5646e58905200cd667db44f022f5e3063a
Oskilla <gael.lode@etu.univ-nantes.fr>
2019-03-29 07:38:29 +0100

    les tests sont réparé, cependant il manque encore les contraintes sur les cardinalité

--- src/main/java/fr/unantes/software/construction/calendar/Calendar.java
+++ src/main/java/fr/unantes/software/construction/calendar/Calendar.java
@@ -1,5 +1,6 @@
 package fr.unantes.software.construction.calendar;
 
+import fr.unantes.software.construction.people.Agent;
 import fr.unantes.software.construction.people.Person;
 
 import java.util.ArrayList;
@@ -11,18 +12,19 @@ import java.util.Vector;
 public class Calendar {
     private final static  int maxtravels = 10;
     private MultipleReferences<Travel> travels = new MultipleBidirectionnalReferencesToCalendar(this, maxtravels);
-    private Person owner;
+    private SingleBidirectionnalReferenceToCalendar owner;
 
-    public Calendar(Person owner) {
-        this.owner = owner;
+    public Calendar(Agent owner) {
+        this.owner = new SingleBidirectionnalReferenceToCalendar(this);
+        this.setOwner(owner);
     }
 
-    public Person getOwner() {
+    public SingleBidirectionnalReferenceToCalendar getOwner() {
         return owner;
     }
 
-    public void setOwner(Person owner) {
-        this.owner = owner;
+    public void setOwner(Agent owner) {
+        this.owner.set(owner);
     }
 
     public boolean addTravel(Travel travel) {
