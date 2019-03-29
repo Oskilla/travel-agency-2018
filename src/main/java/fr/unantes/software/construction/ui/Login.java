37f4068f783fea51ee552d5f4b9538b2a5819006
Yves-Marie CARETTE <E155311R@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-29 11:43:30 +0100

    Gestion du Login

--- src/main/java/fr/unantes/software/construction/ui/Login.java
+++ src/main/java/fr/unantes/software/construction/ui/Login.java
@@ -1,12 +1,18 @@
 package fr.unantes.software.construction.ui;
 
+import fr.unantes.software.construction.people.Administrateur;
+import fr.unantes.software.construction.people.Agent;
+import fr.unantes.software.construction.people.Person;
 import fr.unantes.software.construction.security.UserManager;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class Login {
 
@@ -19,54 +25,82 @@ public class Login {
     private String nom;
     private UserManager bd;
     private Stage stage;
-    private Scene scene;
 
-    public Login(String s, UserManager basededonnee, Stage stage, Scene scene){
+    public Login(String s, UserManager basededonnee, Stage stage) {
         this.bd = basededonnee;
-        this.nom =s;
+        this.nom = s;
         this.stage = stage;
-        this.scene = scene;
     }
 
-    public void login(ActionEvent actionEvent) {
+    public void login(ActionEvent actionEvent) throws Exception {
 
-        if (idutilisateur.getText().isEmpty()){
+        if (idutilisateur.getText().isEmpty()) {
             idmanqueutilisateur.setVisible(true);
-        }
-        else {
+        } else {
             idmanqueutilisateur.setVisible(false);
         }
 
-        if (idmdp.getText().isEmpty()){
+        if (idmdp.getText().isEmpty()) {
             idmanquemdp.setVisible(true);
-        }
-        else{
+        } else {
             idmanquemdp.setVisible(false);
         }
         System.out.println(nom);
 
 
-        if(!idmdp.getText().isEmpty()&&!idutilisateur.getText().isEmpty()) {
+        if (!idmdp.getText().isEmpty() && !idutilisateur.getText().isEmpty()) {
 
             if (bd.getMapMdp().validatePassword(idutilisateur.getText(), idmdp.getText())) {
                 System.out.println("Vous vous connectez avec le compte suivant : " + idutilisateur.getText());
-                stage.setScene(scene);
-            /*
-            if (idmdp.getText() == "mdpsoline"){
 
-            }
-            else if(typeutilisateur == agent) {
-                nouvellevueagent
-            }
-            else (typeutilisateur == administrateur){
-                nouvellevueadministrateur
-            }*/
+                if ((bd.getNamesToUsers().get(idutilisateur.getText())) instanceof Agent) {
 
-            } else {
-                System.out.println("ici");
-                idnoutilisateur.setVisible(true);
+                    ctrlAgent ctrlA = new ctrlAgent(bd, idutilisateur.getText());
+
+                    final URL url3 = getClass().getResource("/views/Agent.fxml");
+                    final FXMLLoader fxmlLoader3 = new FXMLLoader(url3);
+                    fxmlLoader3.setController(ctrlA);
+                    // Chargement du FXML.
+                    final AnchorPane root3 = (AnchorPane) fxmlLoader3.load();
+                    Scene scene3 = new Scene(root3, 800, 800);
+
+                    stage.setScene(scene3);
+
+                    System.out.println("Connexion avec un agent");
+
+
+                } else if ((bd.getNamesToUsers().get(idutilisateur.getText())) instanceof Administrateur) {
+                    System.out.println("Connexion avec un utilisateur");
+
+                } else  if ((bd.getNamesToUsers().get(idutilisateur.getText())) instanceof Person ) {
+
+                    System.out.println("Connexion de Dieu");
+                    //construction du controleur de la vue superUtilisateur
+                    VueSuperUtilisateur vueSuperUtilisateurtest = new VueSuperUtilisateur(bd);
+
+                    //Localisation du fichier FXML
+                    final URL url2 = getClass().getResource("/views/VueSuperUtilisateur.fxml");
+
+                    //Creation du loader
+                    final FXMLLoader fxmlLoader2 = new FXMLLoader(url2);
+
+                    //Affectation du controleur
+                    fxmlLoader2.setController(vueSuperUtilisateurtest);
+
+                    // Chargement du FXML.
+                    final AnchorPane root2 = (AnchorPane) fxmlLoader2.load();
+
+                    Scene scene2 = new Scene(root2, 800, 800);
+
+                    stage.setScene(scene2);
+                }
+                else {
+                    System.out.println("Mauvaise connexion");
+                    idnoutilisateur.setVisible(true);
+                }
             }
+
         }
-
     }
+
 }
