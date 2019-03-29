8366aab1fd282a5039c4cd3cc7a1ab59837c030d
Soline LECOMTE <E164462N@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-29 18:17:08 +0100

    Creation d'une liste de voyages et envoi de celle-ci au controlleur de la fenetre

--- src/main/java/fr/unantes/software/construction/ui/GUI.java
+++ src/main/java/fr/unantes/software/construction/ui/GUI.java
@@ -1,5 +1,9 @@
 package fr.unantes.software.construction.ui;
 
+import fr.unantes.software.construction.calendar.Calendar;
+import fr.unantes.software.construction.calendar.City;
+import fr.unantes.software.construction.calendar.Correspondence;
+import fr.unantes.software.construction.calendar.Travel;
 import fr.unantes.software.construction.people.Administrateur;
 import fr.unantes.software.construction.people.Agent;
 import fr.unantes.software.construction.people.Person;
@@ -11,6 +15,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 
 public class GUI extends Application  {
 
@@ -19,10 +26,12 @@ public class GUI extends Application  {
     private final Person gaelLode;
     private final Person yvesmarieCarette;
     private final UserManager bd;
+    private final ArrayList<Travel> bd2;
     private final Person dieu;
 
     public GUI() throws Exception {
         bd =new UserManager();
+        bd2 = new ArrayList<Travel>();
 
         dieu = new Person("dieu");
         solineLecomte = new Agent("Soline");
@@ -33,6 +42,60 @@ public class GUI extends Application  {
         bd.addUser(solineLecomte, "mdpsoline");
         bd.addUser(gaelLode, "mdpgael");
         bd.addUser(yvesmarieCarette, "mdpyvesmarie");
+
+
+        Calendar cal1 = new Calendar((Agent)solineLecomte);
+        City cit1 = new City ("country1","cname1");
+        City cit2 = new City ("country2", "cname2");
+        City cit3 = new City ("country1","cname3");
+        City cit4 = new City ("country3", "cname4");
+        Date date1 = new Date(1995,12,1);
+        Date date2 = new Date(2005,12,1);
+        Date date3 = new Date(2015,12,1);
+        Date date4 = new Date(2025,12,1);
+        Date date5 = new Date(2035,12,1);
+        Date date6 = new Date(2035,12,1);
+
+
+        Travel trav1 = new Travel(cal1);
+        Correspondence cor1 = new Correspondence(cit1,cit2,date1,date2);
+        Correspondence cor2 = new Correspondence(cit2,cit3, date2,date3);
+        Correspondence[] listcorr = {cor1, cor2};
+        for (Correspondence each : listcorr){
+            trav1.getSteps().add(each);
+        }
+
+        Travel trav2 = new Travel();
+        Correspondence cor3 = new Correspondence(cit2, cit4, date3,date4);
+        Correspondence cor4 = new Correspondence(cit4, cit2, date4,date5);
+        Correspondence[] listcorr2 = {cor3, cor4};
+        for (Correspondence each : listcorr2){
+            trav2.getSteps().add(each);
+        }
+
+        Travel trav3 = new Travel();
+        Correspondence cor5 = new Correspondence(cit3, cit1, date1,date3);
+        Correspondence cor6 = new Correspondence(cit1, cit3, date5,date6);
+        Correspondence[] listcorr3 = {cor5, cor6};
+        for (Correspondence each : listcorr3){
+            trav3.getSteps().add(each);
+        }
+        bd2.add(trav1);
+        bd2.add(trav2);
+        bd2.add(trav3);
+
+
+        ArrayList<Agent> listeAgent = bd.getAgents();
+        Agent courant = listeAgent.get(listeAgent.indexOf(bd.getNamesToUsers().get("Soline")));
+
+        courant.getCalendar().get().getTravels().add(trav1);
+
+
+        Collection<Travel> travels = courant.getCalendar().get().getTravels().get();
+        System.out.println(travels);
+
+
+
     }
 
 
@@ -42,7 +105,7 @@ public class GUI extends Application  {
     public void start(Stage stage) throws Exception {
 
         //construction du controleur du login
-        Login login = new Login("L'interface trop bg de YM", bd, stage);
+        Login login = new Login("L'interface trop bg de YM", bd, bd2, stage);
 
         // Localisation du fichier FXML.<
         final URL url = getClass().getResource("/views/Login.fxml");
@@ -57,7 +120,7 @@ public class GUI extends Application  {
         final AnchorPane root = (AnchorPane) fxmlLoader.load();
 
 
-        Scene scene = new Scene(root, 800, 800);
+        Scene scene = new Scene(root, 600, 400);
         stage.setScene(scene);
         stage.show();
 
