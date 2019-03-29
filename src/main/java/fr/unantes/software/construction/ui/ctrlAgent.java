eb93111cceb734c316deb64b0c830063ac18a6cb
Soline LECOMTE <E164462N@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2019-03-29 18:21:43 +0100

    Mise en place du controller de la vue d'ajout d'un voyage a un agent

--- src/main/java/fr/unantes/software/construction/ui/ctrlAgent.java
+++ src/main/java/fr/unantes/software/construction/ui/ctrlAgent.java
@@ -1,5 +1,6 @@
 package fr.unantes.software.construction.ui;
 
+import fr.unantes.software.construction.calendar.Correspondence;
 import fr.unantes.software.construction.calendar.Travel;
 import fr.unantes.software.construction.calendar.City;
 
@@ -18,63 +19,122 @@ import java.util.Collection;
 import java.util.Date;
 
 public class ctrlAgent {
-    public TableView FCKINGTEST;
-    public TableColumn vDep;
-    public TableColumn pDep;
-    public TableColumn hDep;
-    public TableColumn vArr;
-    public TableColumn pArr;
-    public TableColumn hArr;
+    public TableView tableTravelUser;
+    public TableColumn vDep1;
+    public TableColumn pDep1;
+    public TableColumn hDep1;
+    public TableColumn vArr1;
+    public TableColumn pArr1;
+    public TableColumn hArr1;
+
+    public TableView tableTravel;
+    public TableColumn vDep2;
+    public TableColumn pDep2;
+    public TableColumn hDep2;
+    public TableColumn vArr2;
+    public TableColumn pArr2;
+    public TableColumn hArr2;
 
 
     public TextField test;
     private UserManager bd;
     private String nom;
     private Agent courant;
+    private ArrayList<Travel> bd2;
 
-    public ctrlAgent(UserManager bd, String nom){
+    public ctrlAgent(UserManager bd, ArrayList<Travel> bd2, String nom){
         this.bd = bd;
+        this.bd2 = bd2;
         this.nom = nom;
-
+        ArrayList<Agent> listeAgent = bd.getAgents();
+        this.courant = listeAgent.get(listeAgent.indexOf(bd.getNamesToUsers().get(nom)));
     }
 
 
-    public void afficher(ActionEvent actionEvent) throws InvalidClassException {
-        FCKINGTEST.setEditable(false);
+    public void update() throws InvalidClassException {
+        tableTravelUser.setEditable(false);
+
+        Collection<Travel> voyagesUsers = courant.getCalendar().get().getTravels().get();
 
 
-        ArrayList<Agent> listeAgent = bd.getAgents();
+        System.out.println(voyagesUsers);
+        ArrayList<Voyage> listeVoyagesUser = new ArrayList<>();
 
-
-        courant = listeAgent.get(listeAgent.indexOf(bd.getNamesToUsers().get(nom)));
-        Collection<Travel> travels = courant.getCalendar().getTravels().get();
-        System.out.println(travels);
-
-        ArrayList<Voyage> listeVoyages = new ArrayList<>();
-
-        for(Travel trav : travels){
+        for(Travel trav : voyagesUsers){
             City villeArr = (City)trav.getFirstStep().getStartCity().get();
             City villeDep = (City)trav.getLastStep().getDestinationCity().get();
-            listeVoyages.add(new Voyage(villeDep.getName(),villeDep.getCountry(),trav.getFirstStep().getStartTime(), villeArr.getName(), villeArr.getCountry(), trav.getLastStep().getArrivalTime()));
+            listeVoyagesUser.add(new Voyage(villeDep.getName(),villeDep.getCountry(),trav.getFirstStep().getStartTime(), villeArr.getName(), villeArr.getCountry(), trav.getLastStep().getArrivalTime()));
         }
 
 
-        vDep.setCellValueFactory(new PropertyValueFactory<Voyage,String>("departV"));
-        pDep.setCellValueFactory(new PropertyValueFactory<Voyage,String>("departP"));
-        hDep.setCellValueFactory(new PropertyValueFactory<Voyage,Date>("hDep"));
-        vArr.setCellValueFactory(new PropertyValueFactory<Voyage,String>("arriveeV"));
-        pArr.setCellValueFactory(new PropertyValueFactory<Voyage,String>("arriveeP"));
-        hArr.setCellValueFactory(new PropertyValueFactory<Voyage,Date>("hArr"));
-        ObservableList<Voyage> list1 = FXCollections.observableArrayList(listeVoyages);
+        vDep1.setCellValueFactory(new PropertyValueFactory<Voyage,String>("departV"));
+        pDep1.setCellValueFactory(new PropertyValueFactory<Voyage,String>("departP"));
+        hDep1.setCellValueFactory(new PropertyValueFactory<Voyage,Date>("hDep"));
+        vArr1.setCellValueFactory(new PropertyValueFactory<Voyage,String>("arriveeV"));
+        pArr1.setCellValueFactory(new PropertyValueFactory<Voyage,String>("arriveeP"));
+        hArr1.setCellValueFactory(new PropertyValueFactory<Voyage,Date>("hArr"));
+        ObservableList<Voyage> list1 = FXCollections.observableArrayList(listeVoyagesUser);
 
-
-
-        FCKINGTEST.setItems(list1);
+        tableTravelUser.setItems(list1);
 
 
     }
 
-    public void ajouter(ActionEvent actionEvent) {
-        System.out.println(FCKINGTEST);
+
+    public void afficher2(ActionEvent actionEvent) throws InvalidClassException {
+        tableTravelUser.setEditable(false);
+
+        ArrayList<Voyage> listeVoyages = new ArrayList<>();
+
+        for(Travel travel : bd2){
+            City villeArr = (City)travel.getFirstStep().getStartCity().get();
+            City villeDep = (City)travel.getLastStep().getDestinationCity().get();
+            listeVoyages.add(new Voyage(villeDep.getName(),villeDep.getCountry(),travel.getFirstStep().getStartTime(), villeArr.getName(), villeArr.getCountry(), travel.getLastStep().getArrivalTime()));
+        }
+
+        vDep2.setCellValueFactory(new PropertyValueFactory<Voyage,String>("departV"));
+        pDep2.setCellValueFactory(new PropertyValueFactory<Voyage,String>("departP"));
+        hDep2.setCellValueFactory(new PropertyValueFactory<Voyage,Date>("hDep"));
+        vArr2.setCellValueFactory(new PropertyValueFactory<Voyage,String>("arriveeV"));
+        pArr2.setCellValueFactory(new PropertyValueFactory<Voyage,String>("arriveeP"));
+        hArr2.setCellValueFactory(new PropertyValueFactory<Voyage,Date>("hArr"));
+        ObservableList<Voyage> list2 = FXCollections.observableArrayList(listeVoyages);
+        tableTravel.setItems(list2);
+
     }
+
+    public void ajouter(ActionEvent actionEvent) throws InvalidClassException {
+        Voyage voyage = (Voyage)tableTravel.getSelectionModel().getSelectedItem();
+        System.out.println(voyage);
+        Travel tmp=null;
+        for (Travel t : bd2){
+            for (Correspondence c : t.getSteps().get()){
+                City villeArr = (City)t.getFirstStep().getStartCity().get();
+                City villeDep = (City)t.getLastStep().getDestinationCity().get();
+                System.out.println(villeArr.getName());
+                System.out.println(voyage.getArriveeV());
+                System.out.println(villeArr.getCountry());
+                System.out.println(voyage.getArriveeP());
+                System.out.println(villeDep.getName());
+                System.out.println(voyage.getDepartV());
+                System.out.println(villeDep.getCountry());
+                System.out.println(voyage.getDepartP());
+
+
+                if(voyage.equalsTravel(t,c)){
+                    tmp=t;
+                }
+            }
+        }
+        System.out.println(tmp);
+        if(tmp!=null){
+            System.out.println("la");
+
+            courant.getCalendar().get().addTravel(bd2.get(bd2.indexOf(tmp)));
+        }else{
+            System.out.println("ici");
+        }
+        update();
+    }
+
 }
